33efcac45984f21c89522ebb1598efe1a4c805f2
Paulius Melaika <pmpauliusm@gmail.com>
2020-11-26 15:40:48 +0200

    Added auth

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def signup_user():
     data = request.get_json()
     hashed_password = generate_password_hash(data['password'], method='sha256')
     new_user = Users(public_id=str(uuid.uuid4()),
-                     name=data['name'], password=hashed_password, admin=False)
+                     name=data['email'], password=hashed_password, admin=False)
     db.session.add(new_user)
     db.session.commit()
 
@@ -79,14 +79,14 @@ def signup_user():
 @app.route('/login', methods=['GET', 'POST'])
 def login_user():
 
-    auth = request.authorization
+    auth = request.get_json()
 
-    if not auth or not auth.username or not auth.password:
+    if not auth or not auth['email'] or not auth['password']:
         return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})
 
-    user = Users.query.filter_by(name=auth.username).first()
+    user = Users.query.filter_by(name=auth['email']).first()
 
-    if check_password_hash(user.password, auth.password):
+    if check_password_hash(user.password, auth['password']):
         token = jwt.encode({'public_id': user.public_id, 'exp': datetime.datetime.utcnow(
         ) + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
         return jsonify({'token': token.decode('UTF-8')})
